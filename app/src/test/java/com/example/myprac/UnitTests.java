187907990b12998dd7cc851fa3d8fe4fb2779187
Zafar Yab Subhani <61932990+yaab5825@users.noreply.github.com>
2020-04-28 00:33:27 +0200

    Unit Tests

---
+++ app/src/test/java/com/example/myprac/UnitTests.java
@@ -0,0 +1,26 @@
+package com.example.myprac;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTests
+{
+    @Test
+    public void addition_isCorrect() throws Exception
+    {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test public void mainActivity_shouldNotBeNull() throws Exception
+    {
+        MainActivity mainActivity = new MainActivity();
+        assertNotNull(mainActivity);
+    }
+
+}
