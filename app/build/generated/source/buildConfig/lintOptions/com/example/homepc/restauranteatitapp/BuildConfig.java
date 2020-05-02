c5f97f9c2b37dd1b24bd8a877048321352a59d87
Zafar Yab Subhani <61932990+yaab5825@users.noreply.github.com>
2020-05-02 13:15:25 +0200

    Fianl

---
+++ app/build/generated/source/buildConfig/lintOptions/com/example/homepc/restauranteatitapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.homepc.restauranteatitapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.homepc.restauranteatitapp";
+  public static final String BUILD_TYPE = "lintOptions";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
